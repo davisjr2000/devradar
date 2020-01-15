106ed231628cf68b644d4fdee256dace840fae8b
Davis JR <davisjr2000@hotmail.com>
2020-01-15 13:42:13 -0600

    Comment Function Code

--- backend/src/utils/parseStringAsArray.js
+++ backend/src/utils/parseStringAsArray.js
@@ -1,3 +1,5 @@
 module.exports = function parseStringAsArray(arrayAsString = []) {
-    return arrayAsString.split(',').map(tech => tech.trim());
+    return arrayAsString.split(',').map((tech) => tech.trim());
 }
+
+// This method converts a String to an Array
