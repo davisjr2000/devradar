5490e21e4b869da591035a3930e1c90bf198f723
Davis JR <davisjr2000@hotmail.com>
2020-01-16 10:35:35 -0600

    Add CORS To BackEnd

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes.js');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,7 +10,8 @@ mongoose.connect('mongodb+srv://davisjr2000:omnistack@cluster0-h1igz.mongodb.net
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000);
+app.listen(3333);
